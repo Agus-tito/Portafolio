f0fc10f392a11cee141f192297cece1c3a68899b
Agustin Suarez <titoagustinsuarez6@gmail.com>
2024-02-15 22:18:33 -0300

    arreglando rutas

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://agus-tito.github.io',
-  base: '/mi-repo',
+  base: '/Portafolio',
 });
